c8fb9652d0de8aa980f641bd15d326e731c850fa
AndersonPadovani <andersonpadovani@protonmail.com>
2024-09-29 11:27:32 -0300

    Fix Config Versel

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default defineConfig({
-    test: {
-        setupFiles: ["dotenv/config"],
-    },
+  test: {
+    setupFiles: ["dotenv/config"],
+  },
 });
